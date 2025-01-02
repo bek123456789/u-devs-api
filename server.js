b138fee94698f69f8580f79cf8e5eb849cc0f678
bek123456789 <redmirating@gmail.com>
2025-01-02 15:05:30 +0500

    done

--- server.js
+++ server.js
@@ -13,36 +13,35 @@ const clientRoutes = require('./routes/clientRoutes');
 const app = express();
 const PORT = 3000;
 
-// Middleware
+
 app.use(bodyParser.json());
 
-// CORS Configuration (Allow all origins)
 app.use(cors({
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true, // Allow cookies and other credentials
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }));
 
-// MongoDB Connection
+
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://username:password@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority";
 
-// Remove deprecated options
-mongoose.set('strictQuery', true); // Recommended for modern Mongoose
+
+mongoose.set('strictQuery', true);
 mongoose.connect(MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => {
         console.error('MongoDB connection error:', err.message);
-        process.exit(1); // Exit process if the database connection fails
+        process.exit(1);
     });
 
-// API Routes
+
 app.use('/api/admins', adminRoutes);
 app.use('/api/applications', applicationRoutes);
 app.use('/api/capabilities', capabilityRoutes);
 app.use('/api/cards', cardRoutes);
 app.use('/api/clients', clientRoutes);
 
-// Start Server
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
